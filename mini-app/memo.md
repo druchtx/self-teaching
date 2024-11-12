3eb5472f3dbdfbcad6481ba7be458c95565efb80
hsulzz <xulu@ymail.ne.jp>
2024-11-12 22:16:02 +0900

    backup

---
+++ mini-app/memo.md
@@ -0,0 +1,6 @@
+
+
+[微信小程序文档](https://developers.weixin.qq.com/ebook?action=get_post_info&
+docid=0000286f908988db00866b85f5640a)
+
+
--- mini-app/quick-start/app.js
+++ mini-app/quick-start/app.js
@@ -1,6 +1,12 @@
 // app.js
 App({
-    onLaunch: function () {
-
+    onLaunch: function (options) {
+        console.log('launch options', { options })
+        wx.getSystemInfo({
+            success: (res) => {
+                console.log(res)
+            }
+        })
     }
+
 })
