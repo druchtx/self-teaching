0948e4c0fba5b04765bb6ca76b9897ba9a3a4df6
hsulzz <xulu@ymail.ne.jp>
2024-11-23 14:13:49 +0900

    react

---
+++ react-native/learning-expo/app/_layout.tsx
@@ -0,0 +1,12 @@
+//  _layout 用于定义共用的UI元素，比如herader，tab-bars
+
+import { Stack } from "expo-router";
+
+export default function RootLayout() {
+  return (
+    <Stack>
+      <Stack.Screen name="index" options={{ title: 'Home' }} />
+      <Stack.Screen name="about" options={{ title: 'About' }} />
+    </Stack>
+  );
+}
