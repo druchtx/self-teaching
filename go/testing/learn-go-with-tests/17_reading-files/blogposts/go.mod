831f07e317b4394b3db2fe5c618a079a1b82b6e0
hsulzz <xulu@ymail.ne.jp>
2025-04-16 21:56:57 +0900

    learning reading files

---
+++ go/testing/learn-go-with-tests/17_reading-files/blogposts/go.mod
@@ -0,0 +1,3 @@
+module github.com/hsulzz/blogposts
+
+go 1.24.2
---
+++ go/testing/learn-go-with-tests/17_reading-files/blogposts/post.go
@@ -0,0 +1,63 @@
+package blogposts
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+	"io/fs"
+	"strings"
+)
+
+type Post struct {
+	Title       string
+	Description string
+	Tags        []string
+	Body        string
+}
+
+const (
+	titleSeparator       = "Title: "
+	descriptionSeparator = "Description: "
+	tagsSeparator        = "Tags: "
+)
+
+func getPost(filesystem fs.FS, fileName string) (Post, error) {
+	postFile, err := filesystem.Open(fileName)
+	if err != nil {
+		return Post{}, err
+	}
+	defer postFile.Close()
+	return newPost(postFile)
+
+}
+
+func newPost(postFile io.Reader) (Post, error) {
+	scanner := bufio.NewScanner(postFile)
+
+	readMetaLine := func(tagName string) string {
+		scanner.Scan()
+		return strings.TrimPrefix(scanner.Text(), tagName)
+	}
+
+	title := readMetaLine(titleSeparator)
+	description := readMetaLine(descriptionSeparator)
+	tags := strings.Split(readMetaLine(tagsSeparator), ", ")
+
+	return Post{
+		Title:       title,
+		Description: description,
+		Tags:        tags,
+		Body:        readBody(scanner),
+	}, nil
+
+}
+
+func readBody(scanner *bufio.Scanner) string {
+	scanner.Scan() // ignore a line
+	buf := bytes.Buffer{}
+	for scanner.Scan() {
+		fmt.Fprintln(&buf, scanner.Text())
+	}
+	return strings.TrimSuffix(buf.String(), "\n")
+}
